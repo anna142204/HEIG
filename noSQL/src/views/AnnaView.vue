b18c27e7f4ef752f6be64758989ea56f61ed8147
anna142204 <anna.tranchida@heig-vd.ch>
2024-10-10 12:49:14 +0200

    connexion à la base de donnée

--- noSQL/src/views/AnnaView.vue
+++ noSQL/src/views/AnnaView.vue
@@ -3,8 +3,8 @@ import PouchDB from 'pouchdb'
 export default {
   data() {
     return {
-      datas: [],
-
+      datas: [] as any,
+      database: null as PouchDB.Database | null,
       databaseReference: null
     }
   },
@@ -16,15 +16,50 @@ export default {
     },
 
     initDatabase() {
-      const db = new PouchDB('http://localhost:5986/post')
-      console.log(db)
+      const db = new PouchDB('http://anna:admin@localhost:5984/post')
+      if (db) {
+        console.log("Connected to collection 'post'")
+      } else {
+        console.warn('Something went wrong')
+      }
+      this.database = db
     },
 
-    fetchData() {}
+    fetchData() {
+      if (!this.database) {
+        console.error('Database not initialized')
+        return
+      }
+      try {
+        const result = this.database.allDocs({ include_docs: true })
+
+        this.database
+          .allDocs({
+            include_docs: true
+          })
+          .then((result) => {
+            console.log('hello', result)
+            this.datas = result.rows.map((row) => row.doc) // Stocker les documents dans datas
+
+            console.log('Données stockées dans datas :', this.datas)
+            console.table(this.datas)
+            // handle result
+          })
+          .catch(function (err) {
+            console.log(err)
+          })
+
+        // this.datas = result.rows.map((row) => row.doc)
+        console.log('Données récupérées :', result)
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error)
+      }
+    }
   },
 
   mounted() {
     this.initDatabase()
+    this.fetchData()
   }
 }
 </script>
@@ -32,7 +67,24 @@ export default {
 <template>
   <h1>InfraDon2</h1>
 
-  <!-- <p>Counter: {{ datas }}</p>
+  <!-- Affichage des données récupérées -->
+  <ul v-if="datas.length">
+    <li v-for="(data, index) in datas" :key="index">
+      <p><strong>Nom du post :</strong> {{ data.post_name }}</p>
+      <p><strong>Contenu du post :</strong> {{ data.post_content }}</p>
 
-  <button type="button" role="button" @click="inc">+1</button> -->
+      <!-- Affichage des attributs -->
+      <p><strong>Date de création :</strong> {{ data.attributes.creation_date }}</p>
+      <p><strong>Auteur :</strong> {{ data.attributes.author }}</p>
+
+      <!-- Affichage des commentaires -->
+      <ul>
+        <li v-for="(comment, index) in data.comments" :key="index">
+          <p><strong>Commentaire :</strong> {{ comment.comment }}</p>
+          <p><strong>Auteur du commentaire :</strong> {{ comment.author }}</p>
+        </li>
+      </ul>
+    </li>
+  </ul>
+  <p v-else>Aucune donnée trouvée.</p>
 </template>
