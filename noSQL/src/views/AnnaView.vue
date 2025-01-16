6333f0d8b4f1515cf39073df40d243846f490e71
anna142204 <anna.tranchida@heig-vd.ch>
2025-01-16 10:56:39 +0100

    add and delete media

--- noSQL/src/views/AnnaView.vue
+++ noSQL/src/views/AnnaView.vue
@@ -14,10 +14,13 @@ declare interface Post {
     creation_date: string
     author: string
   }
-  comments: Array<{
-    comment: string
-    author: string
-  }>
+  media: Media[]
+}
+
+declare interface Media {
+  file?: File
+  url: string
+  type: 'image'
 }
 
 export default {
@@ -27,30 +30,86 @@ export default {
       postsData: [] as Post[],
       db: null as PouchDB.Database | null,
       editingPost: null as Post | null,
-      searchTitle: ''
+      searchTitle: '',
+      selectedFile: null as File | null
     }
   },
 
   mounted() {
     this.initDatabase()
-    this.createTitleIndex();
+    this.createTitleIndex()
     this.fetchData()
   },
 
   methods: {
-    async deleteData(id: string, rev: string) {
-      console.log('Call deleteData', rev)
-      const db = ref(this.db).value
-      if (!db) {
-        console.log('Database not valid')
-        return
+    async handleImageUpload(event: Event, postId: string) {
+      const fileInput = event.target as HTMLInputElement
+      if (!fileInput.files || !fileInput.files[0]) return
+
+      const file = fileInput.files[0]
+      const reader = new FileReader()
+
+      reader.onload = async (e) => {
+        if (!e.target?.result) return
+
+        const imageUrl = e.target.result as string
+        const db = this.db
+        // Spécifier explicitement le type de post
+        if (!db) {
+          console.error('Database not valid')
+          return
+        }
+        try {
+          const post = await db.get<Post>(postId)
+
+          // Initialiser le tableau media s'il n'existe pas
+          if (!Array.isArray(post.media)) {
+            post.media = []
+          }
+
+          post.media.push({
+            url: imageUrl,
+            type: 'image'
+          })
+
+          await db.put(post)
+          this.fetchData()
+
+          if (this.editingPost && this.editingPost._id === postId) {
+            this.editingPost = post
+          }
+
+          console.log('Media ajouté avec succès')
+        } catch (error) {
+          console.error("Erreur lors de l'ajout du media:", error)
+        }
       }
+
+      reader.readAsDataURL(file)
+    },
+
+    async removeMedia(postId: string, mediaIndex: number) {
+      const db = ref(this.db).value
+      if (!db) return
+
       try {
-        await db.remove(id, rev)
-        console.log('deleteData success')
+        const post = await db.get<Post>(postId)
+        if (!Array.isArray(post.media)) {
+          post.media = []
+          return
+        }
+
+        post.media.splice(mediaIndex, 1)
+        await db.put(post)
+
         this.fetchData()
+        if (this.editingPost && this.editingPost._id === postId) {
+          this.editingPost = post
+        }
+
+        console.log('Media supprimé avec succès')
       } catch (error) {
-        console.log('deleteData error', error)
+        console.error('Erreur lors de la suppression du media:', error)
       }
     },
 
@@ -61,7 +120,7 @@ export default {
         return
       }
 
-      const fakePosts = Array.from({ length: count }, (_, index) => ({
+      const fakePosts: Omit<Post, '_rev'>[] = Array.from({ length: count }, (_, index) => ({
         _id: new Date().toISOString() + '-' + index,
         post_name: `Post ${index + 1}`,
         post_content: `This is the content for post ${index + 1}.`,
@@ -69,9 +128,11 @@ export default {
           creation_date: new Date().toISOString(),
           author: `Author ${index + 1}`
         },
-        comments: [
-          { comment: `Comment 1 for post ${index + 1}`, author: 'User1' },
-          { comment: `Comment 2 for post ${index + 1}`, author: 'User2' }
+        media: [
+          {
+            url: 'https://picsum.photos/300/201',
+            type: 'image' as const
+          }
         ]
       }))
 
@@ -84,6 +145,27 @@ export default {
       }
     },
 
+    fetchData() {
+  const db = ref(this.db).value;
+  if (!db) {
+    console.error('Database not valid');
+    return;
+  }
+
+  db.allDocs({
+    include_docs: true,
+    attachments: true, 
+  })
+    .then((result: any) => {
+      this.postsData = result.rows
+        .filter((row: any) => !row.id.startsWith('_design/'))
+        .map((row: any) => row.doc);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+}
+,
     putDocument() {
       const document = this.generateFakePosts(1)
       const db = ref(this.db).value
@@ -110,36 +192,36 @@ export default {
       this.editingPost = null
     },
 
-    savePost() {
+    async savePost() {
       const db = ref(this.db).value
       if (!db || !this.editingPost) return
 
-      db.put(this.editingPost)
-        .then(() => {
-          console.log('Post modifié avec succès')
-          this.fetchData() // Recharge les données
-          this.editingPost = null // Réinitialise le mode édition
-        })
-        .catch((error) => {
-          console.error('Erreur lors de la modification du post', error)
-        })
+      try {
+        const post = await db.get(this.editingPost._id)
+        this.editingPost._rev = post._rev
+
+        await db.put(this.editingPost)
+        console.log('Post modifié avec succès')
+        this.fetchData()
+        this.editingPost = null
+      } catch (error) {
+        console.error('Erreur lors de la modification du post', error)
+      }
     },
 
-    fetchData() {
+    async deleteData(id: string, rev: string) {
+      console.log('Call deleteData', rev)
       const db = ref(this.db).value
-      if (db) {
-        db.allDocs
-          .bind(this)({
-            include_docs: true,
-            attachments: true
-          })
-          .then((result: any) => {
-            console.log('fetchData success =>', result.rows)
-            this.postsData = result.rows.map((row: any) => row.doc)
-          })
-          .catch(function (error: any) {
-            console.log('fetchData error', error)
-          })
+      if (!db) {
+        console.log('Database not valid')
+        return
+      }
+      try {
+        await db.remove(id, rev)
+        console.log('deleteData success')
+        this.fetchData()
+      } catch (error) {
+        console.log('deleteData error', error)
       }
     },
 
@@ -189,36 +271,36 @@ export default {
           .on('error', (err: any) => console.error('Sync error', err))
       }
     },
-    
-    async createTitleIndex() {
-  const db = ref(this.db).value;
-  if (!db) {
-    console.error('Database not valid');
-    return;
-  }
-  try {
-    // Liste des index existants
-    const existingIndexes = await db.getIndexes();
-    const indexExists = existingIndexes.indexes.some(
-      (index) => index.name === 'post_name_index'
-    );
 
-    if (!indexExists) {
-      // Crée l'index uniquement s'il n'existe pas
-      await db.createIndex({
-        index: {
-          fields: ['post_name'],
-          name: 'post_name_index', // Nom explicite de l'index
-        },
-      });
-      console.log('Index on "post_name" created successfully');
-    } else {
-      console.log('Index on "post_name" already exists');
-    }
-  } catch (error) {
-    console.error('Error ensuring index:', error);
-  }
-},
+    async createTitleIndex() {
+      const db = ref(this.db).value
+      if (!db) {
+        console.error('Database not valid')
+        return
+      }
+      try {
+        // Liste des index existants
+        const existingIndexes = await db.getIndexes()
+        const indexExists = existingIndexes.indexes.some(
+          (index) => index.name === 'post_name_index'
+        )
+
+        if (!indexExists) {
+          // Crée l'index uniquement s'il n'existe pas
+          await db.createIndex({
+            index: {
+              fields: ['post_name'],
+              name: 'post_name_index' // Nom explicite de l'index
+            }
+          })
+          console.log('Index on "post_name" created successfully')
+        } else {
+          console.log('Index on "post_name" already exists')
+        }
+      } catch (error) {
+        console.error('Error ensuring index:', error)
+      }
+    },
 
     async searchByTitle(title: string) {
       const db = ref(this.db).value
@@ -244,7 +326,6 @@ export default {
       this.searchTitle = '' // Réinitialiser le champ de recherche
       this.fetchData() // Recharger tous les documents
     }
-
   }
 }
 </script>
@@ -260,52 +341,54 @@ export default {
       <button @click="searchByTitle(searchTitle)" class="search-btn">Rechercher</button>
       <button @click="resetSearch" class="reset-btn">Réinitialiser</button>
     </div>
+    <li v-for="post in postsData" :key="post._id" class="post-item">
+      <h2 class="post-title">{{ post.post_name }}</h2>
 
-    <ul>
-      <li v-for="post in postsData" :key="post._id" class="post-item">
-        <div class="post-title">
-          <strong>{{ post.post_name }}</strong>
-          <em class="creation-date" v-if="post.attributes?.creation_date">
-            - {{ post.attributes.creation_date }}
-          </em>
+      <!-- Conteneur pour médias et contenu -->
+      <div class="post-layout">
+        <!-- Section média -->
+        <div v-if="post.media && post.media.length" class="post-media">
+          <div v-for="(mediaItem, index) in post.media" :key="index" class="media-item">
+            <img v-if="mediaItem.type === 'image'" :src="mediaItem.url" alt="Post Image" />
+            <button @click="removeMedia(post._id, index)" class="remove-media-btn">
+              Enlever le média
+            </button>
+          </div>
         </div>
 
-        <p>{{ post.post_content }}</p>
-
-        <!-- Liste des commentaires -->
-        <ul class="comments" v-if="post.comments && post.comments.length">
-          <li v-for="(comment, index) in post.comments" :key="index">
-            <strong>{{ comment.author }}:</strong> {{ comment.comment }}
-          </li>
-        </ul>
-        
-
-        <!-- Actions -->
-        <button @click="deleteData(post._id, post._rev)" class="delete-btn">Supprimer</button>
-        <button @click="editPost(post)" class="edit-btn">Modifier</button>
-
-        <!-- Formulaire d'édition, affiché uniquement pour le post sélectionné -->
-        <div v-if="editingPost && editingPost._id === post._id" class="edit-form">
-          <h2>Modifier le post</h2>
-          <form @submit.prevent="savePost">
-            <label>
-              Nom du post:
-              <input v-model="editingPost.post_name" placeholder="Nom du post" />
-            </label>
-
-            <label>
-              Contenu du post:
-              <textarea v-model="editingPost.post_content" placeholder="Contenu"></textarea>
-            </label>
-
-            <div class="edit-buttons">
-              <button type="submit" class="save-btn">Enregistrer</button>
-              <button type="button" @click="cancelEdit" class="cancel-btn">Annuler</button>
-            </div>
-          </form>
+        <!-- Contenu du post -->
+        <div class="post-content">
+          <p>{{ post.post_content }}</p>
         </div>
-      </li>
-    </ul>
+      </div>
+
+      <!-- Actions -->
+      <button @click="deleteData(post._id, post._rev)" class="delete-btn">Supprimer</button>
+      <button @click="editPost(post)" class="edit-btn">Modifier</button>
+      <!-- Formulaire d'édition, affiché uniquement pour le post sélectionné -->
+      <div v-if="editingPost && editingPost._id === post._id" class="edit-form">
+        <h2>Modifier le post</h2>
+        <form @submit.prevent="savePost">
+          <label>
+            Nom du post:
+            <input v-model="editingPost.post_name" placeholder="Nom du post" />
+          </label>
+
+          <label>
+            Contenu du post:
+            <textarea v-model="editingPost.post_content" placeholder="Contenu"></textarea>
+          </label>
+          <label>
+            Ajouter un média:
+            <input type="file" @change="handleImageUpload($event, editingPost?._id)" />
+          </label>
+          <div class="edit-buttons">
+            <button type="submit" class="save-btn">Enregistrer</button>
+            <button type="button" @click="cancelEdit" class="cancel-btn">Annuler</button>
+          </div>
+        </form>
+      </div>
+    </li>
 
     <button @click="putDocument" class="add-post-btn">Ajouter un post exemple</button>
     <button @click="generateFakePosts(10)" class="add-post-btn">Générer 10 posts exemples</button>
@@ -344,25 +427,78 @@ h2 {
   color: #007bff;
 }
 
+/* Liste des posts */
 .post-item {
   border: 1px solid #ddd;
-  padding: 15px;
-  margin: 10px 0;
-  border-radius: 6px;
-  background-color: #f9f9f9;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin: 20px 0;
+  border-radius: 10px;
+  background-color: #fff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  transition:
+    transform 0.2s,
+    box-shadow 0.2s;
   list-style: none;
 }
 
+.post-item:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+}
+
+/* Titre du post */
 .post-title {
   font-weight: bold;
-  font-size: 1.2em;
+  font-size: 1.5em;
   margin-bottom: 10px;
+  color: #007bff;
 }
 
 .creation-date {
   font-size: 0.9em;
-  color: gray;
+  color: #666;
+}
+/* Conteneur principal des médias et du contenu */
+.post-layout {
+  display: flex;
+  gap: 20px; /* Espacement entre l'image et le contenu */
+  align-items: flex-start; /* Aligner les éléments en haut */
+}
+
+/* Section média */
+.post-media {
+  flex-shrink: 0; /* Empêche la section média de rétrécir */
+  max-width: 200px; /* Limite la largeur des médias */
+}
+
+.post-media img {
+  max-width: 100%; /* Assure que les images ne dépassent pas la largeur de leur conteneur */
+  border-radius: 5px;
+  object-fit: cover;
+}
+
+/* Section contenu */
+.post-content {
+  flex: 1; /* Prend tout l'espace disponible */
+  font-size: 1em;
+  line-height: 1.6;
+  color: #444;
+}
+
+/* Actions */
+.post-actions {
+  margin-top: 15px;
+  display: flex;
+  gap: 10px;
+}
+.remove-media-btn {
+  background-color: #acacac;
+  color: white;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
 }
 
 button {
